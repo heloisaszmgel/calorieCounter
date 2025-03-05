85e01b65f9eeaa042292e516570f843c16de53bb
heloisaszmgel <heloszmgel@gmail.com>
2025-03-04 20:27:16 -0500

    added more functions

--- script.js
+++ script.js
@@ -39,7 +39,12 @@ function getCaloriesFromInputs(list) {
     const currVal = cleanInputString(item.value);
     const invalidInputMatch = isInvalidInput(currVal);
 
-    
+    if (invalidInputMatch) {
+      alert(`Invalid Input: ${invalidInputMatch[0]}`);
+      isError = true
+      return null
+
+    }
   }
 }
 
