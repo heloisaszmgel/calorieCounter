8ac6dfac5b564b23f97ff3219feba30bcf2194c7
heloisaszmgel <heloszmgel@gmail.com>
2025-03-04 20:38:25 -0500

    script updated

--- script.js
+++ script.js
@@ -32,44 +32,17 @@ function addEntry() {
   targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 }
 
-function getCaloriesFromInputs(list) {
-  let calories = 0;
+function calculateCalories(e) {
+  e.preventDefault();
+  isError = false;
 
-  for (const item of list) {
-    const currVal = cleanInputString(item.value);
-    const invalidInputMatch = isInvalidInput(currVal);
-const calorieCounter = document.getElementById('calorie-counter');
-const budgetNumberInput = document.getElementById('budget');
-const entryDropdown = document.getElementById('entry-dropdown');
-const addEntryButton = document.getElementById('add-entry');
-const clearButton = document.getElementById('clear');
-const output = document.getElementById('output');
-let isError = false;
+  const breakfastNumberInputs = document.querySelectorAll("#breakfast input[type='number']");
+  const lunchNumberInputs = document.querySelectorAll("#lunch input[type='number']");
+  const dinnerNumberInputs = document.querySelectorAll("#dinner input[type='number']");
+  const snacksNumberInputs = document.querySelectorAll("#snacks input[type='number']");
+  const exerciseNumberInputs = document.querySelectorAll("#exercise input[type='number']");
 
-function cleanInputString(str) {
-  const regex = /[+-\s]/g;
-  return str.replace(regex, '');
-}
 
-function isInvalidInput(str) {
-  const regex = /\d+e\d+/i;
-  return str.match(regex);
-}
-
-function addEntry() {
-  const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
-  const HTMLString = `
-  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
-  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
-  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
-  <input
-    type="number"
-    min="0"
-    id="${entryDropdown.value}-${entryNumber}-calories"
-    placeholder="Calories"
-  />`;
-  targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 }
 
 function getCaloriesFromInputs(list) {
@@ -84,18 +57,9 @@ function getCaloriesFromInputs(list) {
       isError = true;
       return null;
     }
-    calories += Number(currVal)
+    calories += Number(currVal);
   }
-}
-
-addEntryButton.addEventListener("click", addEntry);
-    if (invalidInputMatch) {
-      alert(`Invalid Input: ${invalidInputMatch[0]}`);
-      isError = true
-      return null
-
-    }
-  }
+  return calories;
 }
 
 addEntryButton.addEventListener("click", addEntry);
